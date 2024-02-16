3f0f3ee9dd211abe0355bcefd98883e61f479172
Ben <ben.oldham2003@gmail.com>
2024-02-16 14:04:05 +0000

    Basic first implementation

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+ 
+# Flask constructor takes the name of current module (__name__) as argument
+app = Flask(__name__)
+ 
+# Decorator which tells the application which URL should call the associated function
+@app.route('/login')
+def login():
+    return render_template('pages/login.html');
+ 
+@app.route('/home')
+def home():
+    return render_template('pages/home.html');
+
+@app.route('/moduleview')
+def moduleView():
+    return render_template('pages/moduleView.html');
+
+@app.route('/lessonview')
+def lessonView():
+    return render_template('pages/lessonView.html');
+
+# Main driver function
+if __name__ == '__main__':
+    # Run the application on the local development server
+    app.run(debug=True)
